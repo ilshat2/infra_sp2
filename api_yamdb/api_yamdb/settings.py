24c7d4789f1315b96e2132c280b7348ab73d338c
Ильшат Кушманбетов <ilshat261093@yandex.ru>
2022-11-01 18:08:59 +0600

    *

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -2,7 +2,7 @@ import os
 
 from datetime import timedelta
 # from dotenv import load_dotenv
-from secret_settings import SECRET_KEY
+# from secret_settings import SECRET_KEY
 
 # load_dotenv()
 
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = KEY
-# SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
+SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
